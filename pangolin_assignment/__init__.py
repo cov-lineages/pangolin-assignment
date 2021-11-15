10300d648ec469b481a1e1158195d091c8680d91
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2021-11-15 16:05:39 +0000

    make cache filename date agnostic and make pangolin assignment installable

---
+++ pangolin_assignment/__init__.py
@@ -0,0 +1,3 @@
+_program = "pangolin-assignment"
+__version__ = "v1.2.91.0"
+__date__ = "2021-11-15"
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import glob
+import os
+import pkg_resources
+# Note: the _program variable is set in __init__.py.
+# it determines the name of the package/final command line tool.
+from pangolin_assignment import __version__, _program
+
+setup(name='pangolin_assignment',
+      version=__version__,
+      packages=find_packages(),
+      scripts=[],
+      package_data={'pangolin_assignment':['*.cache.csv.gz']},
+      description='cached pangolin assignments',
+      url='https://github.com/cov-lineages/pangolin-assignment',
+      author='cov-lineages group',
+      entry_points="""
+      [console_scripts]
+      {program} = pangolin_assignment.command:main
+      """.format(program = _program),
+      include_package_data=True,
+      keywords=[],
+      zip_safe=False)
